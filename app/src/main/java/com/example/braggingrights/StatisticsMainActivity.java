10b3001d7e27ee8805221c89de980b863071c22e
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-11-12 21:59:08 -0700

    created the two stats pages and linked the StatsMainActivity buttons

--- app/src/main/java/com/example/braggingrights/StatisticsMainActivity.java
+++ app/src/main/java/com/example/braggingrights/StatisticsMainActivity.java
@@ -2,7 +2,9 @@ package com.example.braggingrights;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StatisticsMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class StatisticsMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics_main);
+    }
+
+    public void clickViewStatsByGame(View view) {
+        startActivity(new Intent(StatisticsMainActivity.this, ViewStatsByGameActivity.class));
+    }
+
+    public void clickViewStatsByPlayer(View view) {
+        startActivity(new Intent(StatisticsMainActivity.this, ViewStatsByPlayerActivity.class));
     }
 }

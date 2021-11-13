1fdf2ae5afd08f182d43a9a01a794ec25c3144d8
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-11-12 21:54:06 -0700

    created intents for the buttons on Main Activity

--- app/src/main/java/com/example/braggingrights/MainActivity.java
+++ app/src/main/java/com/example/braggingrights/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.braggingrights;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickCreateGame(View view) {
+        // TODO: update intent for create game view
+        startActivity(new Intent(MainActivity.this, CreateGameActivity.class));
+    }
+
+    public void clickCreateLocalPlayer(View view) {
+        // TODO: update intent for create local player view
+        startActivity(new Intent(MainActivity.this, CreateLocalPlayerActivity.class));
+    }
+
+    public void clickViewStatistics(View view) {
+        startActivity(new Intent(MainActivity.this, StatisticsMainActivity.class));
     }
 }

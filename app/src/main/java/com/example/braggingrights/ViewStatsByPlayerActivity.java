7725805e7f407c6a2681e0f592058d581ca964d8
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-11-24 21:53:16 -0700

    Kaylee - statistics presenter; more buttons; started adding functions

--- app/src/main/java/com/example/braggingrights/ViewStatsByPlayerActivity.java
+++ app/src/main/java/com/example/braggingrights/ViewStatsByPlayerActivity.java
@@ -3,6 +3,8 @@ package com.example.braggingrights;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ViewStatsByPlayerActivity extends AppCompatActivity {
 
@@ -11,4 +13,21 @@ public class ViewStatsByPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stats_by_player);
     }
+
+    public void getIndividualStats(View view) {
+        //TODO: Create a spinner that can be populated by our individuals list
+        //String selectedGame = mySpinner.getSelectedItem().toString();
+
+
+        TextView textViewWinPercentage = findViewById(R.id.winToLossPercentageOutput);
+        textViewWinPercentage.setText("Hello");
+
+        TextView textViewTotalWins = findViewById(R.id.totalWinsOutput);
+        textViewTotalWins.setText("Hello");
+    }
+
+    // List of functions we can use:
+    // calculateIndividualPercentage()
+    // calculateIndividualStreak()
+
 }

c036d823ebf3c30c236ec4cb5564b12ca0b676ae
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-11-29 16:44:20 -0700

    Kaylee - updated button on create local player

--- app/src/main/java/com/example/braggingrights/CreateLocalPlayerActivity.java
+++ app/src/main/java/com/example/braggingrights/CreateLocalPlayerActivity.java
@@ -3,6 +3,8 @@ package com.example.braggingrights;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class CreateLocalPlayerActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CreateLocalPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_local_player);
+    }
+
+    public void clickCreatePlayer(View view) {
+        //TODO: send info to create a player
+
+        Toast.makeText(CreateLocalPlayerActivity.this, "Successfully created player", Toast.LENGTH_LONG).show();
     }
 }

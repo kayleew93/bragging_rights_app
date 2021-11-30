644e69c8ab41b92e1e14dcb9a26ec4acef8ef826
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-11-30 11:36:47 -0700

    Kaylee - added initial code for connection to database

--- app/src/main/java/com/example/braggingrights/Storage.java
+++ app/src/main/java/com/example/braggingrights/Storage.java
@@ -1,8 +1,21 @@
 package com.example.braggingrights;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public interface Storage {
 
     //TODO: Write to cloud; return void
+    public static void writeToCloud() {
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference firstNameDB = database.getReference("firstName");
+        firstNameDB.setValue("value");
+
+        DatabaseReference lastNameDB = database.getReference("lastName");
+        lastNameDB.setValue("value");
+    }
 
     //TODO: Retrieve from cloud; return string
 }

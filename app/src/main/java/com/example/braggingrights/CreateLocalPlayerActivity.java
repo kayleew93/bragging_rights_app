864815c1aa33c7102a0d9b2373a5d3c0dc4cabd2
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-12-01 14:31:40 -0700

    Kaylee - added phone number; reformatted some of create player page; added phone number and set games for player

--- app/src/main/java/com/example/braggingrights/CreateLocalPlayerActivity.java
+++ app/src/main/java/com/example/braggingrights/CreateLocalPlayerActivity.java
@@ -7,14 +7,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateLocalPlayerActivity extends AppCompatActivity implements Storage {
 
     // Variables
-    TextView fullName, pNickname;
+    TextView fullName, pNickname, phoneNumber;
     // male/female; avatar
     FirebaseDatabase rootNode;
     DatabaseReference reference;
@@ -27,6 +26,7 @@ public class CreateLocalPlayerActivity extends AppCompatActivity implements Stor
         // hooks all xml elements in activity_create_local_player.xml
         fullName = findViewById(R.id.playerName);
         pNickname = findViewById(R.id.playerNickname);
+        phoneNumber = findViewById(R.id.phoneNumber);
     }
 
     public void clickCreatePlayer(View view) {
@@ -36,8 +36,11 @@ public class CreateLocalPlayerActivity extends AppCompatActivity implements Stor
         // Get the values
         String playerFullName = fullName.getText().toString();
         String playerNickname = pNickname.getText().toString();
+        String playerPhoneNumber = phoneNumber.getText().toString();
+        Integer totalGames = 0;
+        Integer gamesWon = 0;
 
-        FirebaseStorage firebaseStorage = new FirebaseStorage(playerFullName, playerNickname);
+        FirebaseStoragePlayer firebaseStorage = new FirebaseStoragePlayer(playerFullName, playerNickname, playerPhoneNumber, totalGames, gamesWon);
 
         reference.child(playerNickname).setValue(firebaseStorage);
 
---
+++ app/src/main/java/com/example/braggingrights/FirebaseStoragePlayer.java
@@ -0,0 +1,57 @@
+package com.example.braggingrights;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseStoragePlayer {
+
+    String fullName, playerNickname, phoneNumber;
+    Integer totalGames, gamesWon;
+
+    public FirebaseStoragePlayer() {
+    }
+
+    public FirebaseStoragePlayer(String fullName, String playerNickname, String phoneNumber, Integer totalGames, Integer gamesWon) {
+        this.fullName = fullName;
+        this.playerNickname = playerNickname;
+        this.phoneNumber = phoneNumber;
+        this.totalGames = totalGames;
+        this.gamesWon = gamesWon;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPlayerNickname() {
+        return playerNickname;
+    }
+
+    public void setPlayerNickname(String playerNickname) {
+        this.playerNickname = playerNickname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String fullName) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getTotalGames() { return totalGames; }
+
+    public void setTotalGames(Integer totalGames) { this.totalGames = totalGames; }
+
+    public Integer getGamesWon() { return gamesWon; }
+
+    public void setGamesWon(Integer gamesWon) { this.gamesWon = gamesWon; }
+
+}

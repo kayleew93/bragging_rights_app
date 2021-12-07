4c4fadcf9418f134dbc4b36fd4f1c2a2afbb23bb
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-12-07 14:27:47 -0700

    Kaylee - New game templates can now be added to database

--- app/src/main/java/com/example/braggingrights/AddNewGameTemplate.java
+++ app/src/main/java/com/example/braggingrights/AddNewGameTemplate.java
@@ -5,16 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class AddNewGameTemplate extends AppCompatActivity {
+
+    DBHelper db;
+    EditText name, numberRounds;
+    Switch needsDice, highScoreWins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_game_template);
+
+        db = new DBHelper(this);
+        // hooks all xml elements in activity_create_local_player.xml
+        name = findViewById(R.id.editGameName);
+        numberRounds = findViewById(R.id.selectNumberRounds);
+        needsDice = findViewById(R.id.needsDice);
+        highScoreWins = findViewById(R.id.isHighScoreWins);
     }
     public void cancelTemplate(View view) {
         startActivity(new Intent(AddNewGameTemplate.this, CreateGameActivity.class));
+    }
+
+    public void clickAddNewTemplate(View view) {
+
+        String nameTXT = name.getText().toString();
+        String numberRoundsTXT = numberRounds.getText().toString();
+        int numberRoundsINT = Integer.parseInt(numberRoundsTXT);
+        String highScoreWinsTXT = highScoreWins.getText().toString();
+        Boolean highScoreWinsBool = Boolean.parseBoolean(highScoreWinsTXT);
+        String needsDiceTXT = needsDice.getText().toString();
+        Boolean needsDiceBool = Boolean.parseBoolean(needsDiceTXT);
+
+        Boolean checkinsertdata = db.insertNewGameTemplate(nameTXT, numberRoundsINT, highScoreWinsBool, needsDiceBool);
+        if (checkinsertdata==true) {
+            Toast.makeText(AddNewGameTemplate.this, "New Game Template Submitted", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(AddNewGameTemplate.this, CreateGameActivity.class)); }
+        else {
+            Toast.makeText(AddNewGameTemplate.this, "Error: Game template not inserted", Toast.LENGTH_LONG).show();}
     }
 }
 
--- app/src/main/java/com/example/braggingrights/DBHelper.java
+++ app/src/main/java/com/example/braggingrights/DBHelper.java
@@ -21,7 +21,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE PlayerDetails (gamesWon int, gender int, name text, nickname text, phoneNumber int, totalGames int, playerId INTEGER PRIMARY KEY AUTOINCREMENT)");
-        db.execSQL("CREATE TABLE GameDetails (gameId INTEGER PRIMARY KEY AUTOINCREMENT, name text, numberPlayers int, scoreMethod int)");
+        db.execSQL("CREATE TABLE GameTemplate (gameTemplateId INTEGER PRIMARY KEY AUTOINCREMENT, name text, numberRounds int, highScoreWins Boolean, needsDice Boolean)");
         db.execSQL("CREATE TABLE GameResults (gameResultsId INTEGER PRIMARY KEY AUTOINCREMENT, gameId int, FOREIGN KEY (gameId) REFERENCES GameDetails (gameId))");
         db.execSQL("CREATE TABLE PlayersList (gamePlayersListId INTEGER PRIMARY KEY AUTOINCREMENT, gameId int, playerId int, score int, win Boolean, FOREIGN KEY (gameId) REFERENCES GameResults (gameResultsId), FOREIGN KEY (playerId) REFERENCES PlayerDetails (playerId))");
     }
@@ -29,7 +29,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(("DROP TABLE IF EXISTS PlayerDetails"));
-        db.execSQL(("DROP TABLE IF EXISTS GameDetails"));
+        db.execSQL(("DROP TABLE IF EXISTS GameTemplate"));
         db.execSQL(("DROP TABLE IF EXISTS GameResults"));
         db.execSQL(("DROP TABLE IF EXISTS PlayersList"));
         onCreate(db);
@@ -45,6 +45,21 @@ public class DBHelper extends SQLiteOpenHelper {
         contentValues.put("totalGames", totalGames);
         contentValues.put("gamesWon", gamesWon);
         long result = db.insert("PlayerDetails", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean insertNewGameTemplate(String name, int numberRounds, Boolean highScoreWins, Boolean needsDice) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("numberRounds", numberRounds);
+        contentValues.put("highScoreWins", highScoreWins);
+        contentValues.put("needsDice", needsDice);
+        long result = db.insert("GameTemplate", null, contentValues);
         if (result == -1) {
             return false;
         } else {

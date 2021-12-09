e38e70acfd2398c0a3bea9224f2f78971faebf56
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-12-08 20:52:06 -0700

    Kaylee - Removed Firebase and AWS database code

--- app/src/main/java/com/example/braggingrights/MainActivity.java
+++ app/src/main/java/com/example/braggingrights/MainActivity.java
@@ -8,41 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import com.amplifyframework.AmplifyException;
-import com.amplifyframework.core.Amplify;
-import com.amplifyframework.datastore.AWSDataStorePlugin;
-import com.amplifyframework.datastore.generated.model.Player;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        try {
-            //Amplify.addPlugin(new AWSApiPlugin()); // UNCOMMENT this line once backend is deployed
-            Amplify.addPlugin(new AWSDataStorePlugin());
-            Amplify.configure(getApplicationContext());
-            Log.i("Amplify", "Initialized Amplify");
-        } catch (
-        AmplifyException error) {
-            Log.e("Amplify", "Could not initialize Amplify", error);
-        }
-
-        Player item = Player.builder()
-                .fullName("Lorem ipsum dolor sit amet")
-                .nickname("Lorem ipsum dolor sit amet")
-                .totalGames(1020)
-                .gamesWon(1020)
-                .build();
-        Amplify.DataStore.save(
-                item,
-                success -> Log.i("Amplify", "Saved item: " + success.item().getId()),
-                error -> Log.e("Amplify", "Could not save item to DataStore", error)
-        );
-
     }
 
     public void clickCreateGame(View view) {

f0d1679a8b17206ad5a3b4c4abd64c98e861e12b
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2021-12-09 20:30:27 -0700

    Kaylee - Got random number generator working; not the prettiest layout, but it's there

--- app/src/main/java/com/example/braggingrights/DiceActivity.java
+++ app/src/main/java/com/example/braggingrights/DiceActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -13,11 +14,17 @@ import java.util.List;
 
 public class DiceActivity extends AppCompatActivity {
 
+    EditText numberSides, numberDice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice);
 
+        numberDice = findViewById(R.id.numberDiceInput);
+        numberSides = findViewById(R.id.numberSidesInput);
+
+        /*
         // set spinner for the number of dice
         Spinner mySpinnerNumDice = (Spinner) findViewById(R.id.spinnerNumberDice);
 
@@ -33,19 +40,25 @@ public class DiceActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.sidesnumbers));
         myAdapterNumSides.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinnerNumSides.setAdapter(myAdapterNumSides);
-
+        */
     }
 
     public void generateDiceList(View view) {
+        /*
         Spinner mySpinnerDice = (Spinner) findViewById(R.id.spinnerNumberDice);
         Integer spinnerNumberDice = (Integer) mySpinnerDice.getSelectedItem();
 
         Spinner mySpinnerSides = (Spinner) findViewById(R.id.spinnerNumberDice);
         Integer spinnerNumberSides = (Integer) mySpinnerSides.getSelectedItem();
-        
+        */
+        String numberDiceString = numberDice.getText().toString();
+        int numberDiceInt = Integer.parseInt(numberDiceString);
+        String numberSidesString = numberSides.getText().toString();
+        int numberSidesInt = Integer.parseInt(numberSidesString);
+
         // send user-selected values to generate the numbers to a list
         RandomGeneratorPresenter generatorPresenter = new RandomGeneratorPresenter();
-        List diceValues = generatorPresenter.generateRandomNumberSet(spinnerNumberDice, spinnerNumberSides);
+        List diceValues = generatorPresenter.generateRandomNumberSet(numberDiceInt, numberSidesInt);
 
         // output the list for viewers
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Collections.singletonList(diceValues.toString()));
